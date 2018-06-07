7147934a47983863640256a7f9d6aea211ffd71f
Lucas Heredia <lucash@motorola.com>
2018-06-06 23:23:51 -0300

    k-medians and fuzzy c-means

--- trab_final.R
+++ trab_final.R
@@ -5,8 +5,10 @@
 ########################################
 
 # Libraries
+# install.packages(c('cluster', 'NLP', 'e1071'))
 library(cluster)
 library(NLP)
+library(e1071)
 
 # Set Working Dir
 setwd("/Users/wfr005/work/courses/inf-0613/t_final")
@@ -31,24 +33,55 @@ features.pca2.summary <- summary(features.pca2)
 # Sampling data to develop solution - only 10% of rows - comment to have full results
 set.seed(42); features.sample <- features[sample(1:nrow(features), nrow(features) * 0.1, replace = FALSE),]
 features.sample.pca <- prcomp(features.sample, scale. = FALSE)
+features.sample.pca.dist <- dist(features.sample.pca$x[,1:1390])
 
-# Kmeans clustering
+# k-means clustering
 features.sample.kmeans.5 <- kmeans(features.sample.pca$x[,1:1390], 5, nstart = 20)
 features.sample.kmeans.10 <- kmeans(features.sample.pca$x[,1:1390], 10, nstart = 20)
 features.sample.kmeans.15 <- kmeans(features.sample.pca$x[,1:1390], 15, nstart = 20)
 features.sample.kmeans.20 <- kmeans(features.sample.pca$x[,1:1390], 20, nstart = 20)
-
-# Silhouette
-features.sample.kmeans.5.sil <- silhouette(features.sample.kmeans.5$cluster, dist(features.sample.pca$x[,1:1390]))
-features.sample.kmeans.10.sil <- silhouette(features.sample.kmeans.10$cluster, dist(features.sample.pca$x[,1:1390]))
-features.sample.kmeans.15.sil <- silhouette(features.sample.kmeans.15$cluster, dist(features.sample.pca$x[,1:1390]))
-features.sample.kmeans.20.sil <- silhouette(features.sample.kmeans.20$cluster, dist(features.sample.pca$x[,1:1390]))
+# k-means silhouette
+features.sample.kmeans.5.sil <- silhouette(features.sample.kmeans.5$cluster, features.sample.pca.dist)
+features.sample.kmeans.10.sil <- silhouette(features.sample.kmeans.10$cluster, features.sample.pca.dist)
+features.sample.kmeans.15.sil <- silhouette(features.sample.kmeans.15$cluster, features.sample.pca.dist)
+features.sample.kmeans.20.sil <- silhouette(features.sample.kmeans.20$cluster, features.sample.pca.dist)
 
 mean(features.sample.kmeans.5.sil[,3])
 mean(features.sample.kmeans.10.sil[,3])
 mean(features.sample.kmeans.15.sil[,3])
 mean(features.sample.kmeans.20.sil[,3])
 
+# k-medians
+features.sample.kmedians.5 <- kcca(features.sample.pca$x[,1:1390], 5, family=kccaFamily("kmedians"))
+features.sample.kmedians.10 <- kcca(features.sample.pca$x[,1:1390], 10, family=kccaFamily("kmedians"))
+features.sample.kmedians.15 <- kcca(features.sample.pca$x[,1:1390], 15, family=kccaFamily("kmedians"))
+features.sample.kmedians.20 <- kcca(features.sample.pca$x[,1:1390], 20, family=kccaFamily("kmedians"))
+# k-medians silhouette
+features.sample.kmedians.5.sil <- silhouette(clusters(features.sample.kmedians.5), features.sample.pca.dist)
+features.sample.kmedians.10.sil <- silhouette(clusters(features.sample.kmedians.10), features.sample.pca.dist)
+features.sample.kmedians.15.sil <- silhouette(clusters(features.sample.kmedians.15), features.sample.pca.dist)
+features.sample.kmedians.20.sil <- silhouette(clusters(features.sample.kmedians.20), features.sample.pca.dist)
+# avg
+summary(features.sample.kmedians.5.sil )$avg.width
+summary(features.sample.kmedians.10.sil)$avg.width
+summary(features.sample.kmedians.15.sil)$avg.width
+summary(features.sample.kmedians.20.sil)$avg.width
+
+# Fuzzy c-means
+features.sample.cmeans.5 <- cmeans(features.sample.pca$x[,1:1390], 5, m = 2)
+features.sample.cmeans.10 <- cmeans(features.sample.pca$x[,1:1390], 10, m = 2)
+features.sample.cmeans.15 <- cmeans(features.sample.pca$x[,1:1390], 15, m = 2)
+features.sample.cmeans.20 <- cmeans(features.sample.pca$x[,1:1390], 20, m = 2)
+# k-medians silhouette
+features.sample.cmeans.5.sil <- silhouette(features.sample.cmeans.5$cluster, features.sample.pca.dist)
+features.sample.cmeans.10.sil <- silhouette(features.sample.cmeans.10$cluster, features.sample.pca.dist)
+features.sample.cmeans.15.sil <- silhouette(features.sample.cmeans.15$cluster, features.sample.pca.dist)
+features.sample.cmeans.20.sil <- silhouette(features.sample.cmeans.20$cluster, features.sample.pca.dist)
+# avg
+summary(features.sample.kmedians.5.sil )$avg.width
+summary(features.sample.kmedians.10.sil)$avg.width
+summary(features.sample.kmedians.15.sil)$avg.width
+summary(features.sample.kmedians.20.sil)$avg.width
 
 # Bigrams calculation
 get_bigram_freq <- function(headlines) {
